d9c1f43a7b19b709face5a578905dce4b4e96d62
magnuskurtto <magnus.kurtto@unifaun.com>
2021-06-03 22:20:33 +0200

    Response from async function.

--- service/journal.js
+++ service/journal.js
@@ -2,45 +2,51 @@ const csv = require("csvtojson");
 const { Readable } = require("stream");
 const db = require("../database");
 
+async function saveToDB(fileData) {
+    await csv({
+        delimiter: ";"
+    })
+    .fromStream(Readable.from(fileData))
+    .subscribe( csvRow =>  {
+        try {
+            db.query(`INSERT INTO registry (
+                personalNumber, 
+                firstName, 
+                lastName, 
+                birthDay, 
+                address
+            ) 
+            values ($1, $2, $3, $4, $5)
+            ON CONFLICT (personalNumber)
+            DO UPDATE SET
+            firstName = $2,
+            lastName = $3,
+            birthDay = $4,
+            address = $5`, Object.values(csvRow));
+        } catch(e) {
+            console.error(e.message);
+            response.status(400).send("Failed to execute database query")
+        }
+    });
+}
+
 module.exports = { 
     doImport: (request, response) => {
-        console.log("DO IMPORT");
-
         if (!request.files || Object.keys(request.files).length === 0) {
-            return response.status(400).send("No files where uploaded");
+            response.status(400).send("No files where uploaded");
         }
 
         const file = request.files.csvfile;
 
         if (!file.name.endsWith(".csv")) {
-            return response.status(400).send("Only csv files are supported");
+            response.status(400).send("Only csv files are supported");
         }
 
-        csv({
-            delimiter: ";"
-        })
+        (async () => {
+            await saveToDB(file.data);
+            response.sendStatus(200);
+        })()
         
-        .fromStream(Readable.from(file.data))
-        .subscribe( csvRow =>  {
-            try {
-                db.query(`INSERT INTO registry (
-                    personalNumber, 
-                    firstName, 
-                    lastName, 
-                    birthDay, 
-                    address
-                ) 
-                values ($1, $2, $3, $4, $5)
-                ON CONFLICT (personalNumber)
-                DO UPDATE SET
-                firstName = $2,
-                lastName = $3,
-                birthDay = $4,
-                address = $5`, Object.values(csvRow));
-            } catch(e) {
-                console.error(e.message);
-            }
-        }).on("done", error => error ? response.status(error.status) : response.status(200));
     },
 
     getJournals: async (request, response) => {
@@ -50,7 +56,7 @@ module.exports = {
             response.status(200).send(result.rows);
         } catch(e) {
             console.error(e.message)
-            response.status(e.status).send("Could not read journals");
+            response.status(e.status).send("Could not read from registry");
         }
     }
 
